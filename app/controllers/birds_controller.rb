c056183e92dd64679251f0e5cc9e7dfc7d03c9a5
Josekariz <joseph.macharia@student.moringaschool.com>
2022-11-22 11:15:41 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,6 @@
 class BirdsController < ApplicationController
+  #doing this for wrap parameters  for this controller but for overall controllers go tothe config folder with the wrap parameters.
+   wrap_parameters format: []
 
   # GET /birds
   def index
@@ -6,20 +8,39 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+    # GET /birds/:id
+    def show
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        render json: bird
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
+
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(name: params[:name], species: params[:species])
+    
+    #>>>>>>>>>>>>>>>>>
+    #this is one way of doing it. using strong params
+    
+    # bird = Bird.create(params.permit(:name, :species))
+
+    #>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    #Refactoring params
+
+    bird = Bird.create(bird_params)
+
+byebug 
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
+  private
+    def bird_params
+      params.permit(:name, :species)
     end
-  end
+
 
 end
